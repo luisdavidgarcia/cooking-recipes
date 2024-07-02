2588b65ec9bc56cc2ea9d8388efa5ebd3a46b402
Luis David Garcia <hookeri_bartz@aleeas.com>
2024-07-01 21:28:42 -0700

    made about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,20 @@
+export default function AboutPage() {
+    return(
+        <div>
+            <h1>About</h1>
+            <p>
+                Hi there, I'm Luis. I'm a software engineer, and I love to cook. 
+                I created this site to share my cooking adventures with you. I 
+                will admit presentation is not my strong suit, but by the end 
+                of the day, it's all about the taste. I hope you enjoy the 
+                recipes and start your own cooking adventures too. 
+            </p>
+            <p>
+                Most of the recipes I share here are in form of picture, recipe, 
+                and personal ramble, so feel free to skip the last section if 
+                you desire. Also I will often give credit to where I got the 
+                inspiration from, as we should share one another's love for food.
+            </p>
+        </div>
+    );
+}
